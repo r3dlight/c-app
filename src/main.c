c5b25f7983e00010d1f378f434edb69d6327cbde
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-09-08 14:03:53 +0200

    Remove rand test

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
 
 #include <stdio.h>
-#include <rand.h>
 
 int main(void)
-{   int random_number;
-    random_number = shield_rand();
+{   
     printf("Hello, World from sample C app\n");
-    printf("Random number is %d.\n", random_number);
+    //printf("Random number is %d.\n", random_number);
     return 0;
 }
