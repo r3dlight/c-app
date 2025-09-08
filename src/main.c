5ddb97da2ae48853387b4e3012cb92f675441709
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-09-08 13:51:23 +0200

    Add simple random num declaration

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(void)
-{
+{   int random_number;
     random_number = shield_rand();
     printf("Hello, World from sample C app\n");
     printf("Random number is %d.\n", random_number);
