bf0bf96662d568decea00b20618986393ba010ab
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-09-09 07:22:56 +0200

    Test rand() fn

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {   
+    int random_number;
+    random_number = rand();
     printf("Hello, World from sample C app\n");
-    //printf("Random number is %d.\n", random_number);
+    printf("Random number is %d.\n", random_number);
     return 0;
 }
